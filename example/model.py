78d5d9c70a8a889ad50748a99854269ab1785fa0
brucejh99 <bjhong@yahoo.com>
2020-10-31 19:52:42 -0400

    Removed GRU since LSTM has built-in gating

--- example/model.py
+++ example/model.py
@@ -1,31 +1,8 @@
 import numpy as np
 import tensorflow as tf
 import magenta
-from numpy import array
 from tensorflow import keras
 from tensorflow.keras import layers
-
-
-
-# Reading data. LSTM expects 3D array as input
-def process_data(file_path):
-    # raw_data = list(map(lambda x: x.split(','), open(file_path).read().split('\n')))
-    data = array([
-        [0.1, 1.0],
-        [0.2, 0.9],
-        [0.3, 0.8],
-        [0.4, 0.7],
-        [0.5, 0.6],
-        [0.6, 0.5],
-        [0.7, 0.4],
-        [0.8, 0.3],
-        [0.9, 0.2],
-        [1.0, 0.1]])
-    # reshapes sequence into 3D 
-    data.reshape((1, 10, 2))
-    print(data.shape)
-
-process_data('dataset/shampoo_sales.csv')
 
 
 # This creates a LSTM RNN according to the parameters we give it.
